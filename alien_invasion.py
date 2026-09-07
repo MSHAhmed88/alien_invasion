b3f205f583594a1bdabd62f1703de5fe985fe43f
Mohammad Ahmed <mohammadsh.ahmed88@gmail.com>
2026-09-07 14:45:01 +0100

    Creating a clock for controlling the frame rate

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ class AlienInvasion:
     def __init__(self):
         """Initialising the game, and create resources"""
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
@@ -21,6 +22,7 @@ class AlienInvasion:
             
             # make the most recently drawn screen visible, i.e. updates the game window.
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # make a game instance, and run the game.
