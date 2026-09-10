527610faf10f0dc6ac45c2b172d3a7fbd6598717
Mohammad Ahmed <mohammadsh.ahmed88@gmail.com>
2026-09-10 16:58:59 +0100

    Adding Play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats 
+from button import Button 
 from ship import Ship
 from bullet import Bullet
 from alien import Alien 
@@ -35,7 +36,10 @@ class AlienInvasion:
         self._create_fleet()
 
         #start Alien Invasion in an active state.
-        self.game_active = True 
+        self.game_active = False 
+
+        #make the play button.
+        self.play_button = Button(self, "Play")
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -59,6 +63,14 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type ==pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player clicks Play"""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.game_active = True 
 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
@@ -191,6 +203,10 @@ class AlienInvasion:
             bullet.draw_bullet()
         self.ship.blitme()
         self.aliens.draw(self.screen)
+
+        #draw the play button if the game is inactive.
+        if not self.game_active:
+            self.play_button.draw_button()
 
         # make the most recently drawn screen visible, i.e. updates the game window.
         pygame.display.flip()
